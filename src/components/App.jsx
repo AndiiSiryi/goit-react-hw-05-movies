1d111692b5b10fa4e348b62f1693b6bdb02659ee
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2023-11-11 19:27:09 +0200

    Layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
-import styles from './App.module.css';
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import PageContainer from './PageContainer/PageContainer';
 import Loader from './Loader/Loader';
+import Layout from './Layout/Layout';
 
 // import Home from 'pages/Home/Home';
 // import MovieDetails from 'pages/MovieDetails/MovieDetails';
@@ -18,32 +18,20 @@ const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 export const App = () => {
   return (
-    <>
-      <header>
-        <nav className={styles.nav}>
-          <NavLink to="/" className={styles.navLink}>
-            Home
-          </NavLink>
-          <NavLink to="/movies" className={styles.navLink}>
-            Movies
-          </NavLink>
-        </nav>
-      </header>
-      <main>
-        <PageContainer>
-          <Suspense fallback={<Loader />}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/movies" element={<Movies />} />
-              <Route path="/movies/:movieId" element={<MovieDetails />}>
-                <Route path="cast" element={<Cast />} />
-                <Route path="reviews" element={<Reviews />} />
-              </Route>
-              <Route path="*" element={<Home />} />
-            </Routes>
-          </Suspense>
-        </PageContainer>
-      </main>
-    </>
+    <Layout>
+      <PageContainer>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:movieId" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </Suspense>
+      </PageContainer>
+    </Layout>
   );
 };
